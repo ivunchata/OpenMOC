2f5b611c125a1af544bd0399154ac166833c6955
Ivan Tonkov <ivan.tonkov@gmail.com>
2015-09-28 00:24:31 +0300

    removed HexPlane since it was replaced by Hexagon

--- src/Surface.h
+++ src/Surface.h
@@ -296,39 +296,6 @@ public:
   std::string toString();
 };
 
-/**
- * @class XPlane Surface.h "src/Surface.h"
- * @brief Represents an inclined Plane that is a part of a regular hexagon
- */
-class HexPlane: public Plane {
-
-private:
-    /** A 2D point for the Hexagon center */
-    Point _center;
-
-    /** The Hexagon's radius */
-    double _radius;
-
-    /** Number of the Plane on the Hexagon */
-    /** 0 means top left, 1 - left and so on...*/
-    size_t _side_num;
-
-    /** The two points, fixing the plane */
-    Point _vertex[2];
-
-    /** Min and Max coords if applicable for each halfspace */
-    /** 0 corresponds to halfspace -1, and 1 to halfspace 1*/
-    double _min_x[2], _max_x[2], _min_y[2], _max_y[2];
-
-public:
-  HexPlane(const double x, const double y, const double radius,
-           const size_t hex_id, const int id=0, const char* name="");
-  double getMinX(int halfspace);
-  double getMaxX(int halfspace);
-  double getMinY(int halfspace);
-  double getMaxY(int halfspace);
-
-};
 
 /**
  * @class Circle Surface.h "src/Surface.h"
